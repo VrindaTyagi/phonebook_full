2745fbb1bd0874ca7aad3355cf6f0bbbc5f98cc1
VrindaTyagi <vrindatyagi09@gmail.com>
2021-03-03 17:15:32 +0530

    all good now

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const app = require('./app') // the actual Express application
-const http = require('http')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
+const app = require("./app"); // Good Express application
+const http = require("http");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 const morgan = require("morgan");
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 app.use(morgan(":method :url :status :res[header] - :response-time ms :data"));
 
@@ -15,7 +15,6 @@ morgan.token("data", function getId(req) {
   });
 });
 
-
 server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
-})
+  logger.info(`Server running on port ${config.PORT}`);
+});
